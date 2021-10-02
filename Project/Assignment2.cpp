730c748a726981673e0291c0525e877a278fcf26
Erich Ellsworth <kg5key@kg5key.com>
2021-10-01 20:45:04 -0500

    Outline of program. Basic menu.

---
+++ Project/Assignment2.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+
+
+using namespace std;
+
+struct Movie {
+    int id;
+    string name, releaeDate;
+    double rating;
+};
+
+// Rubric required function constructors. DO NOT EDIT.
+void readMovies(ifstream& in, Movie movies[], int& size);  // in is a file stream to "movies.txt" file, set in main
+void displayMovies(Movie movies[], int SIZE);
+void sortByDateRating(Movie movies[], int SIZE);
+void sortById(Movie movies[], const int SIZE);
+int linearSearchByName(Movie movies[], int SIZE, string targetName);
+
+// Custom constructors.
+void displayMenu();
+
+int main() {
+
+    ifstream in;
+    in.open("movies.txt");
+
+    Movie movies[100];
+    int size = 0;
+
+    int userInput = 5;
+
+    while (userInput != 5) {
+        displayMenu();
+
+        cin >> userInput;
+    }
+
+    readMovies(in, movies, size);
+
+
+    in.close();
+}
+
+void displayMenu() {
+    cout << "\nMenu\n"
+         << "1. Display movies sorted by id\n"
+         << "2. Display movies sorted by release date, then rating\n"
+         << "3. Lookup a release date given a name\n"
+         << "4. Lookup a movie by id\n"
+         << "5. Quit the Program\n"
+         << "Enter your choice:\n";
+}
+
+void readMovies(ifstream& in, Movie movies[], int& size) {
+
+  
+
+    // Movie file format
+    // ID
+    // NAME
+    // DATE
+    // RATING
+    // BLANK
+
+    int id;
+    string name, date;
+    double rating; 
+
+    while (in >> id) {
+        in >> ws;       // skips the newline after the number in the file
+        getline(in, name);
+        cout << id << " " << name << endl;  
+
+
+    }
+
+
+
+}
