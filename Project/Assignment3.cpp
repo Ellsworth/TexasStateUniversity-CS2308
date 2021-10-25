7e4d24b8366f38ab75d9d242f8f5c056411a7052
Erich Ellsworth <kg5key@kg5key.com>
2021-10-25 17:18:43 -0500

    Basic read example.

--- Project/Assignment3.cpp
+++ Project/Assignment3.cpp
@@ -19,9 +19,9 @@ struct Bakery {
     int quantity;
     double price;
 
-
 };
 
+// Rubric required function constructors. DO NOT EDIT.
 void readBakery(ifstream& in,Bakery **bakeries, int N, int M);
 void displayMenu();
 void displayBakeryItems(Bakery **bakeries, int N, int M);
@@ -33,7 +33,46 @@ void displayBakery(const Bakery& bakery);
 void releaseMemory(Bakery**, int, int);
 Bakery** allocateMemory(int N, int M);
 
+// custom constructors
+void readBakeryMetadata(ifstream& in, int &numBakeries, int &numItems);
+
+
+
 int main() {
+
+    ifstream in;
+    in.open("bakery.txt");
+
+    int userInput = 5;
+
+    int numBakeries = 0, numItems = 0;
+
+    readBakeryMetadata(in, numBakeries, numItems);
+
+    Bakery** arr = allocateMemory(numBakeries, numItems);
+    
+
+    cout << numBakeries << " " << numItems << endl;
+
+    readBakery(in, arr, numBakeries, numItems);
+
+    //displayBakeryItems(arr, numBakeries, numItems);
+
+    
+    while (userInput != 5) {
+
+        cin >> userInput;
+
+        switch (userInput) {
+            case 5:
+                break;
+        }
+
+    }
+
+    releaseMemory(arr, numBakeries, numItems);
+
+
 
 }
 
@@ -54,4 +93,97 @@ void displayMenu(){
          << "4. Purchase items\n"
          << "5. Quit the Program\n"
          << "Enter your choice:\n";
+}
+
+void readBakery(ifstream& in,Bakery **bakeries, int numBakeries, int numItems) {
+
+    int currentBakery = 0, currentItem = 0;
+    string blankline;
+
+    while(getline(in,blankline)) {
+
+        if(!blankline.size());//cout << "BREAK" << endl;
+        else {
+            bakeries[currentBakery][currentItem].id = stoi(blankline);
+            getline(in, bakeries[currentBakery][currentItem].name);
+            in >> bakeries[currentBakery][currentItem].quantity;
+            in >> bakeries[currentBakery][currentItem].price;
+
+            cout << "Bakery " << currentBakery + 1 << "/" << numBakeries << " Item " << currentItem + 1 << "/" << numItems << endl;
+
+            cout << "ID       : " << bakeries[currentBakery][currentItem].id << endl;
+            cout << "Name     : " << bakeries[currentBakery][currentItem].name << endl;
+            cout << "Quantity : " << bakeries[currentBakery][currentItem].quantity << endl;
+            cout << "Price    : " << bakeries[currentBakery][currentItem].price << endl;
+
+            currentItem++;
+        }
+
+        if (currentItem == numItems) {
+            currentItem = 0;
+            currentBakery++;
+        }
+
+    }
+
+
+}
+
+void readBakeryMetadata(ifstream& in, int &numBakeries, int &numItems) {
+
+    string blankline;
+
+    in >> numBakeries;
+    in >> numItems;
+
+    return;
+}
+
+
+Bakery** allocateMemory(int N, int M) {
+
+    Bakery** arr = new Bakery*[N]; //1st dimension
+
+	for(int i = 0; i<N ; i++){
+		arr[i] = new Bakery[M];
+	}
+
+    return arr;
+
+}
+
+void releaseMemory(Bakery** arr, int N, int M) {
+
+    for(int i = 0; i < N; i++) {
+        delete [] arr[i];
+    }
+    delete [] arr;
+}
+
+/**
+
+void displayBakeryItems(Bakery bakeries, int N, int M) {
+
+        for (int i = 0; i < N; i++) { // loop for every bakery
+
+        for (int j = 0; j < M; j++) { // loop for every item
+
+            
+
+
+        }
+
+    }
+
+}
+**/
+
+
+void displayBakery(const Bakery& bakery){
+    cout << left
+         << setw(3) << bakery.id
+         << setw(26) << bakery.name
+         << setw(12) << bakery.quantity
+         << fixed << setprecision(1) << bakery.price
+         << endl;
 }
